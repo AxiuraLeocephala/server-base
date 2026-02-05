3ad6e9e43594d152e5fb428a731dcfbac1ecf87a
axiura <jomunyts@gmail.com>
2026-02-05 19:53:15 +0300

    .

--- root/main.py
+++ root/main.py
@@ -5,5 +5,6 @@ from root.presentation.routes import routes
 server = web.Application()
 server.add_routes(routes)
 
+
 def start_server() -> None:
     web.run_app(server, host="127.0.0.1", port=8080)
